cbd84acf11916cadfdacfe3bdac189e49c5c1c92
mateuszskulimowski <skulimowski.mateusz98@gmail.com>
2023-07-24 19:48:39 +0200

    add gh pages and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule } from '@angular/router';
 import { CreatePlanComponent } from './components/create-plan/create-plan.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { LoginComponent } from './components/login/login.component';
+import { TrainingComponent } from './components/training/training.component';
 import { CreatePlanComponentModule } from './components/create-plan/create-plan.component-module';
 import { RegistrationComponentModule } from './components/registration/registration.component-module';
 import { LoginComponentModule } from './components/login/login.component-module';
+import { TrainingComponentModule } from './components/training/training.component-module';
 
 @NgModule({
-  imports: [RouterModule.forRoot([{ path: 'create-plan', component: CreatePlanComponent }, { path: 'register', component: RegistrationComponent }, { path: 'login', component: LoginComponent }]), CreatePlanComponentModule, RegistrationComponentModule, LoginComponentModule],
+  imports: [RouterModule.forRoot([{ path: 'create-plan', component: CreatePlanComponent }, { path: 'register', component: RegistrationComponent }, { path: 'login', component: LoginComponent }, { path: 'plans', component: TrainingComponent }]), CreatePlanComponentModule, RegistrationComponentModule, LoginComponentModule, TrainingComponentModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
