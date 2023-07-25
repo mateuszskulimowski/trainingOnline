823f8374ec21c324130520dca765091c537502cc
mateuszskulimowski <skulimowski.mateusz98@gmail.com>
2023-07-25 17:13:27 +0200

    navigation basic

--- src/app/components/navigation/navigation.component-module.ts
+++ src/app/components/navigation/navigation.component-module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { NavigationComponent } from './navigation.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
-  imports: [MatToolbarModule, RouterModule],
+  imports: [MatToolbarModule, RouterModule, MatButtonModule],
   declarations: [NavigationComponent],
   providers: [],
   exports: [NavigationComponent],
